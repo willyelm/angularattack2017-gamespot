b61be4444b616632cc016b077e52017f0d8a8f69
navyae <navyaeetaram17@gmail.com>
2017-04-23 17:58:18 -0500

    new gh-pages version

--- out-tsc/environments/environment.prod.js
+++
@@ -1,4 +0,0 @@
-export var environment = {
-    production: true
-};
-//# sourceMappingURL=/Users/navyaeetaram/PhpstormProjects/angularattack2017-gamespotgit/src/environments/environment.prod.js.map
